02cbdcdca4479637773a1cb6d998acebbeb7f005
Michael Nolan <mtnolan2640@gmail.com>
2021-05-19 12:51:07 -0400

    Port PipeStageTests

--- src/test/scala/util/PipeStageTest.scala
+++ src/test/scala/util/PipeStageTest.scala
@@ -11,51 +11,33 @@ import matchers._
 
 class PipeStageTest extends AnyFlatSpec with should.Matchers {
 
-  // it should "create verilog" in {
-  //   println("Before unit test?")
-  //   val stage = new ChiselStage
-
-  //   println(stage.emitVerilog(new Delay))
-  //   // println(stage.emitVerilog(new RegisteredPipeStage[UInt, UInt, PipeStage[UInt, UInt]](new Delay)))
-  //   // println(stage.emitVerilog(new RegisteredPipeStage[AdderInput, UInt, PipeStage[AdderInput, UInt]](new Adder(8))))
-  // }
-  // it should "propagate ready/valid" in {
-  // }
+  it should "create verilog" in {
+    SpinalVerilog(new Delay(UInt(8 bits)))
+  }
 
   it should "propagate ready/valid" in {
     SimConfig.withWave.doSim(new Delay(UInt(8 bits))){dut =>
-      //dut.clockDomain.forkStimulus(period = 10)
+      dut.pipeOutput.flush #= false
+      dut.pipeInput.valid #= true
+      dut.pipeOutput.ready #= false
+      sleep(10)
+      assert(dut.pipeOutput.valid.toBoolean == true)
+      assert(dut.pipeInput.ready.toBoolean == false)
+
+      sleep(10)
+      dut.pipeInput.valid #= false
+      dut.pipeOutput.ready #= true
+      sleep(10)
+      assert(dut.pipeOutput.valid.toBoolean == false)
+      assert(dut.pipeInput.ready.toBoolean == true)
+      sleep(10)
+      dut.pipeInput.payload #= 10
+      sleep(10)
+      assert(dut.pipeInput.payload.toInt == 10)
+
+
     }
   }
 
-    // ToyMemSim.main(Array(""))
-  // }
-
-  // it should "propagate ready/valid" in {
-  //   test(new Delay) 
-  //     .withFlags(Array("--t-write-vcd")) { c =>
-  //       c.pipeInput.valid.poke(1.B)
-  //       c.pipeOutput.ready.poke(0.B)
-  //       c.pipeOutput.valid.expect(1.B)
-  //       c.pipeInput.ready.expect(0.B)
-  //       c.clock.step(1)
-
-  //       c.pipeInput.valid.poke(0.B)
-  //       c.pipeOutput.ready.poke(1.B)
-  //       c.pipeInput.ready.expect(1.B)
-  //       c.pipeOutput.valid.expect(0.B)
-
-  //       c.clock.step(1)
-
-  //       c.pipeInput.bits.poke(10.U)
-  //       c.pipeOutput.bits.expect(10.U)
-
-  //       c.clock.step(1)
-
-
-  //       c.clock.step(1)
-
-  //     }
-  // }
 
 }
