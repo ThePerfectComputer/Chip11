05e8f23edcf5c719e2e91ef1ece4fe8314a69d46
Michael Nolan <mtnolan2640@gmail.com>
2021-05-20 16:31:55 -0400

    Use new one-hot variety of switch in Mux1H

--- src/main/scala/util/Mux1H.scala
+++ src/main/scala/util/Mux1H.scala
@@ -9,23 +9,44 @@ object Mux1H {
 
   def apply[T <: Data](oneHot : BitVector,inputs : Vec[T]): T = apply(oneHot.asBools,inputs)
   def apply[T <: Data](oneHot : collection.IndexedSeq[Bool],inputs : Vec[T]): T = {
+    val oneHotBits = oneHot.asBits
     var maxWidth :Int = 0
-    for(item <- inputs){
-      if(item.getBitsWidth > maxWidth){
-        maxWidth = item.getBitsWidth
+      for(item <- inputs){
+        if(item.getBitsWidth > maxWidth){
+          maxWidth = item.getBitsWidth
+        }
+      }
+      val outBits = Bits(maxWidth bits)
+      outBits := 0
+
+    oneHotSwitch(oneHotBits){
+      for((item, i) <- inputs.zipWithIndex){
+        is(i){
+          outBits := item.asBits
+        }
       }
     }
-    val muxedVec = Vec(Bits(maxWidth bits), inputs.size)
-    for((item, idx) <- inputs.zipWithIndex){
-      val muxed = Bits(maxWidth bits).setPartialName(s"muxed_$idx")
-      muxed := Mux(oneHot(idx), inputs(idx).asBits, B(0, maxWidth bits))
-      muxedVec(idx) := muxed
-    }
-    val outBits = Bits(maxWidth bits)
-    outBits := muxedVec.reduceLeft(_ | _)
+      val out = cloneOf(inputs(0))
+      out.assignFromBits(outBits)
+      out
 
-    val out = cloneOf(inputs(0))
-    out.assignFromBits(outBits)
-    out
+    // var maxWidth :Int = 0
+    // for(item <- inputs){
+    //   if(item.getBitsWidth > maxWidth){
+    //     maxWidth = item.getBitsWidth
+    //   }
+    // }
+    // val muxedVec = Vec(Bits(maxWidth bits), inputs.size)
+    // for((item, idx) <- inputs.zipWithIndex){
+    //   val muxed = Bits(maxWidth bits).setPartialName(s"muxed_$idx")
+    //   muxed := Mux(oneHot(idx), inputs(idx).asBits, B(0, maxWidth bits))
+    //   muxedVec(idx) := muxed
+    // }
+    // val outBits = Bits(maxWidth bits)
+    // outBits := muxedVec.reduceLeft(_ | _)
+
+    // val out = cloneOf(inputs(0))
+    // out.assignFromBits(outBits)
+    // out
   }
 }
--- src/test/scala/OneHotTest.scala
+++ src/test/scala/OneHotTest.scala
@@ -20,6 +20,23 @@ class OneHotDUT(items: Int, width: Int) extends Component {
   io.out_decoded := Mux1H(io.in_oh, decodeItems).resized
 }
 
+class OneHotSwitch(items: Int, width: Int) extends Component {
+  val io = new Bundle {
+    val in_oh = in Bits(items bits)
+    val out_decoded = out UInt(width bits)
+  }
+
+  io.out_decoded := io.out_decoded.getZero
+  val decodeItems = Seq.tabulate(items)(i => U((i+1)*(i+1), width bits))
+  oneHotSwitch(io.in_oh){
+    for((item, i) <- decodeItems.zipWithIndex){
+      is(i){
+        io.out_decoded := item
+      }
+    }
+  }
+}
+
 object TestEnum extends SpinalEnum {
   val e0, e1, e2, e3, e4, e5, e6, e7 = newElement()
   val e8, e9, e10, e11, e12, e13, e14, e15 = newElement()
@@ -52,6 +69,7 @@ class OneHotTest extends AnyFlatSpec with should.Matchers {
   it should "create verilog" in {
     val config = SpinalConfig(mode=Verilog, mergeAsyncProcess=true).withoutEnumString()
     config.generate(new OneHotDUT(16, 12))
+    config.generate(new OneHotSwitch(16, 12))
     config.generate(new OneHotEnum(12))
   }
 }
