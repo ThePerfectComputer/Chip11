9c123047289ff9223ec5d4be3e4adfbce0b2ffbb
Michael Nolan <mtnolan2640@gmail.com>
2021-05-21 10:14:07 -0400

    Port register files

---
+++ src/main/scala/cpu/shared/Regfiles.scala
@@ -0,0 +1,74 @@
+package cpu.shared
+
+import cpu.interfaces.regfile.{ReadPort, WritePort, WritePortMasked}
+
+import spinal.core._
+import spinal.lib._
+
+class Regfile(numRegs: Int, regWidth: Int, readPorts:Int, writePorts:Int) extends Component {
+  val idxWidth = log2Up(numRegs)
+  val io = new Bundle{
+    val rp = Vec(new ReadPort(idxWidth, regWidth), readPorts)
+
+    val wp = Vec(new WritePort(idxWidth, regWidth), writePorts)
+  }
+
+  val mem = Mem(UInt(regWidth bits), numRegs)
+
+  for(port <- io.rp) {
+    port.data := mem.readSync(port.idx)
+  }
+  for(port <- io.wp) {
+    mem.write(port.idx, port.data, enable=port.en)
+  }
+}
+
+class RegfileMasked(numRegs: Int, regWidth: Int, readPorts:Int, writePorts:Int, maskWidth: Int) extends Component {
+  val idxWidth = log2Up(numRegs)
+
+  val elemWidth = regWidth/maskWidth // The width of each element that can be masked
+
+  val io = new Bundle{
+    val rp = Vec(new ReadPort(idxWidth, regWidth), readPorts)
+
+    val wp = Vec(new WritePortMasked(idxWidth, regWidth, maskWidth), writePorts)
+  }
+
+  val mem = Mem(Bits(regWidth bits), numRegs)
+
+  for(port <- io.rp) {
+    port.data := mem.readSync(port.idx).asBits.asUInt
+  }
+  for(port <- io.wp) {
+    mem.write(port.idx, port.data.asBits, enable=port.en, mask=port.mask.asBits)
+  }
+}
+
+
+// Another Multi-Port regfile that should synthesize a bit better
+class BRAMMultiRegfile(numRegs: Int, regWidth: Int, readPorts:Int, writePorts:Int) extends
+    Regfile(numRegs, regWidth, readPorts, writePorts){
+
+  val memIdxBits = log2Up(writePorts)
+  val memArr = for (i <- 0 until writePorts) yield Mem(UInt(regWidth bits), numRegs)
+
+
+  // Create a table holding the memory that was most recently written to for each register
+  val lvt = Mem(UInt(memIdxBits bits), numRegs)
+
+
+  for((port, i) <- io.rp.zipWithIndex) {
+    val mem_idx = UInt(memIdxBits bits)
+    mem_idx := lvt.readSync(port.idx)
+
+    val read_data = Vec(UInt(regWidth bits), writePorts)
+    for((mem, idx) <- memArr.zipWithIndex) read_data(idx) := mem.readSync(port.idx)
+
+    port.data := read_data(mem_idx)
+  }
+
+  for((port, i) <- io.wp.zipWithIndex) {
+    lvt.write(port.idx, i)
+    memArr(i).write(port.idx, port.data, enable=port.en)
+  }
+}
