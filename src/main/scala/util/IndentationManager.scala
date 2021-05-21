e5fc9dd5573d6f881f12bb4209d26cff9f79b259
Michael Nolan <mtnolan2640@gmail.com>
2021-05-21 18:53:35 -0400

    Make indentationmanager like yoshi wants

--- src/main/scala/util/IndentationManager.scala
+++ src/main/scala/util/IndentationManager.scala
@@ -1,16 +1,18 @@
 package util
 
 class IndentationManager(val spacesPerLevel: Int=4){
-  var indentationLevel:Int = 0
+  val tabs = " ".repeat(spacesPerLevel)
 
-  def tabs : String = {
-    " ".repeat(indentationLevel*spacesPerLevel)
-  }
-  def indent(levels:Int=1){
-    indentationLevel += levels
-  }
-  def dedent(levels:Int=1){
-    indentationLevel -= levels
-    if(indentationLevel < 0) indentationLevel = 0
+  def getIndent(levels:Int=1)(body: IndentationManager=> Unit) : Unit ={
+    val newManager = new IndentationManager(spacesPerLevel)
+    val stream = new java.io.ByteArrayOutputStream()
+    Console.withOut(stream) {
+      body(newManager)
+    }
+    val lines = stream.toString().split("\n")
+    for(line <- lines){
+      println(s"$tabs$line")
+    }
   }
 }
+
--- src/test/scala/util/IndentationManagerTest.scala
+++ src/test/scala/util/IndentationManagerTest.scala
@@ -4,47 +4,26 @@ import org.scalatest._
 import flatspec._
 import matchers._
 
-
 class IndentationManagerTest extends AnyFlatSpec with should.Matchers {
   behavior of "IndentationManager"
 
-  val manager = new IndentationManager
+  it should "support context manager" in {
 
-  it should "print a blank string by default" in {
-    val result = manager.tabs
-    assert(result == "")
-  }
+    val stream = new java.io.ByteArrayOutputStream()
+    Console.withOut(stream) {
+      val manager = new IndentationManager
+      println("No Indent")
+      manager.getIndent() { manager =>
+        println("More Indent")
+        manager.getIndent() { manager =>
+          println("Most Indent")
+        }
+      }
+    }
+    assert(stream.toString() =="""No Indent
+    More Indent
+        Most Indent
+""")
 
-  it should "indent once" in {
-    manager.indent()
-    assert(manager.tabs == "    ")
-  }
-  it should "indent more than once" in {
-    manager.indent(2)
-    assert(manager.tabs == "            ")
-  }
-  it should "dedent once" in {
-    manager.dedent()
-    assert(manager.tabs == "        ")
-  }
-  it should "dedent more than once" in {
-    manager.dedent(2)
-    assert(manager.tabs == "")
-  }
-  it should "not do anything when at indentation level 0" in {
-    manager.dedent()
-    assert(manager.tabs == "")
-    manager.indent()
-    assert(manager.tabs == "    ")
-  }
-
-  it should "support variable indentation width" in {
-    val manager2 = new IndentationManager(2)
-    manager2.indent()
-    assert(manager2.tabs == "  ")
-    manager2.indent(2)
-    assert(manager2.tabs == "      ")
-    manager2.dedent()
-    assert(manager2.tabs == "    ")
   }
 }
