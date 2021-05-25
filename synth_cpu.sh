7c5e3340ecdb937543bec217285ed6accf60c9a9
Michael Nolan <mtnolan2640@gmail.com>
2021-05-25 12:37:43 -0400

    Update synthesis script

--- synth_cpu.sh
+++ synth_cpu.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 sbt "test:testOnly *SoCTestVerilog"
-yosys -p 'synth_ecp5 -json cpu.json' CPU.v
-nextpnr-ecp5 --json cpu.json --out-of-context --85k --freq 100
+yosys -p 'synth_ecp5 -json cpu.json -retime' CPU.v
+nextpnr-ecp5 --json cpu.json --out-of-context --85k --freq 100 --router router2
