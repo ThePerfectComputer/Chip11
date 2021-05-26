1db4426cd2c7a899016fcae83841a3645e541834
Michael Nolan <mtnolan2640@gmail.com>
2021-05-26 14:56:14 -0400

    Delete retime in synth script

--- synth_cpu.sh
+++ synth_cpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 sbt "test:testOnly *SoCTestVerilog"
-yosys -p 'synth_ecp5 -json cpu.json -retime' CPU.v
-nextpnr-ecp5 --json cpu.json --out-of-context --85k --freq 100 --router router2
+yosys -p 'synth_ecp5 -json cpu.json' CPU.v
+nextpnr-ecp5 --json cpu.json --out-of-context --85k --freq 100 --router router2 2>&1 | tee pnr.log
