c42478f18ca1b66a0077ad81709feb635e66e541
Michael Nolan <mtnolan2640@gmail.com>
2021-05-31 09:13:32 -0400

    Update SoCTest and fmax_notes

--- src/test/scala/soc/SoCTest.scala
+++ src/test/scala/soc/SoCTest.scala
@@ -119,7 +119,7 @@ class SoCTestVerilog extends AnyFlatSpec with should.Matchers {
   it should "create verilog" in {
     implicit val config = new CPUConfig(
       adder = true,
-      branch = false,
+      branch = true,
       logical = true,
       shifter = false,
       comparator = true,
