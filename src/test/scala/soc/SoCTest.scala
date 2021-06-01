db98a2cbbe03d45e0b7cc51861ef7c9d0c52b781
Michael Nolan <mtnolan2640@gmail.com>
2021-06-01 11:44:08 -0400

    Have ldst response be directly input

--- src/test/scala/soc/SoCTest.scala
+++ src/test/scala/soc/SoCTest.scala
@@ -77,6 +77,8 @@ class CPUShiftRegDUT(implicit val config: CPUConfig) extends Component {
     val tdo = out Bool
     val tck = in Bool
     val sample = in Bool
+    val fetch_status = in(TransactionStatus())
+    val ldst_status = in(TransactionStatus())
   }
   val cpu = new CPU
   class ResponseBundle extends Bundle {
@@ -96,6 +98,9 @@ class CPUShiftRegDUT(implicit val config: CPUConfig) extends Component {
     cpu.io.ldst_request.ldst_req
   )
 
+  val fetch_status_reg = RegNext(io.fetch_status)
+  val ldst_status_reg = RegNext(io.fetch_status)
+
   val input_reg = Reg(Bits(inputs.getBitsWidth bits))
   val output_reg = Reg(Bits(outputs.getWidth bits))
   inputs.assignFromBits(input_reg)
@@ -111,13 +116,8 @@ class CPUShiftRegDUT(implicit val config: CPUConfig) extends Component {
   when(io.sample) {
     output_reg := outputs
   }
-  when(io.en) {
-    cpu.io.fetch_response.status := TransactionStatus.DONE
-    cpu.io.ldst_response.status := TransactionStatus.DONE
-  }.otherwise {
-    cpu.io.fetch_response.status := TransactionStatus.WAITING
-    cpu.io.ldst_response.status := TransactionStatus.WAITING
-  }
+  cpu.io.fetch_response.status := fetch_status_reg
+  cpu.io.ldst_response.status := ldst_status_reg
 }
 
 class SoCTestVerilog extends AnyFlatSpec with should.Matchers {
