80e803549946b5c603961b50bcc2d519d5fcbefa
Michael Nolan <mtnolan2640@gmail.com>
2021-06-02 11:49:25 -0400

    Change MemBusToAXI to a state machine

--- src/main/scala/soc/devices/memory_adaptor/MemBusToAXI.scala
+++ src/main/scala/soc/devices/memory_adaptor/MemBusToAXI.scala
@@ -17,6 +17,7 @@ import cpu.shared.memory_state.{
 
 import spinal.core._
 import spinal.lib._
+import spinal.lib.fsm._
 import scala.collection.concurrent.Debug
 
 class MemBusToAXIShared(id: Int)(implicit axiConfig: Axi4Config)
@@ -46,9 +47,9 @@ class MemBusToAXIShared(id: Int)(implicit axiConfig: Axi4Config)
   io.axi.arw.len := 0
   io.axi.arw.id := id
 
-  io.axi.w.data := 0
-  io.axi.w.strb := 0
   io.axi.w.last := False
+  io.axi.w.payload.data := io.membus.write_data.asBits
+  io.axi.w.strb := io.membus.write_mask.asBits
 
   io.membus.read_data := io.membus.read_data.getZero
   io.membus.status := TransactionStatus.WAITING
@@ -58,42 +59,65 @@ class MemBusToAXIShared(id: Int)(implicit axiConfig: Axi4Config)
     3 downto 0
   )
 
-  val pastTransaction = RegInit(TransactionType.NONE)
-  pastTransaction := TransactionType.NONE
-  when(io.axi.arw.valid & io.axi.arw.ready) {
-    pastTransaction := io.membus.ldst_req
-  }
-  when(io.membus.ldst_req === TransactionType.LOAD) {
-    io.axi.arw.write := False
-    io.axi.arw.valid := True
-  }
-  when(io.membus.ldst_req === TransactionType.STORE) {
-    io.axi.arw.write := True
-    io.axi.arw.valid := True
-    io.axi.w.valid := True
-    io.axi.w.last := True
-    io.axi.w.payload.data := io.membus.write_data.asBits
-    io.axi.w.strb := io.membus.write_mask.asBits
-  }
-  when(io.axi.arw.valid & io.axi.arw.ready) {
-    io.membus.req_ack := True
-  }
+  val fsm = new StateMachine {
+    def doIdle {
+      when(io.membus.ldst_req === TransactionType.LOAD) {
+        io.axi.arw.write := False
+        io.axi.arw.valid := True
+        when(io.axi.arw.ready) {
+          goto(stateLoad)
+          io.membus.req_ack := True
+        }
+      }
+      when(io.membus.ldst_req === TransactionType.STORE) {
+        io.axi.arw.write := True
+        io.axi.arw.valid := True
+        io.axi.w.valid := True
+        io.axi.w.last := True
+        when(io.axi.arw.ready) {
+          goto(stateStoreData)
+        }
+        when(io.axi.arw.ready & io.axi.w.ready) {
+          goto(stateStoreResp)
+          io.membus.req_ack := True
+        }
+      }
 
-  when(pastTransaction === TransactionType.LOAD) {
-    io.axi.r.ready := True
-    when(io.axi.r.ready & io.axi.r.valid) {
-      io.membus.read_data.assignFromBits(io.axi.r.payload.data)
-      io.membus.status := TransactionStatus.DONE
+    }
+    val stateIdle: State = new State with EntryPoint {
+      whenIsActive {
+        doIdle
+      }
+    }
+    val stateLoad: State = new State {
+      whenIsActive {
+        io.axi.r.ready := True
+        when(io.axi.r.valid) {
+          io.membus.read_data.assignFromBits(io.axi.r.payload.data)
+          io.membus.status := TransactionStatus.DONE
+          goto(stateIdle)
+          doIdle
+        }
+      }
+    }
+    val stateStoreResp: State = new State {
+      whenIsActive {
+        io.axi.b.ready := True
+        when(io.axi.b.valid) {
+          io.membus.status := TransactionStatus.DONE
+          goto(stateIdle)
+          doIdle
+        }
+      }
+    }
+    val stateStoreData: State = new State {
+      whenIsActive {
+        io.axi.w.valid := True
+        when(io.axi.w.ready) {
+          io.membus.req_ack := True
+          goto(stateIdle)
+        }
+      }
     }
   }
-  when(pastTransaction === TransactionType.STORE) {
-    io.axi.b.ready := True
-    when(io.axi.b.ready & io.axi.b.valid) {
-      io.membus.status := TransactionStatus.DONE
-    }
-  }
-  when(pastTransaction === TransactionType.NONE) {
-    io.membus.status := TransactionStatus.IDLE
-  }
-
 }
