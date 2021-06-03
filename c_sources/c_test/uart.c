005d88af50fe876299d0c47ddca97e547eb0611b
Michael Nolan <mtnolan2640@gmail.com>
2021-06-03 15:45:29 -0400

    Simplify uart.c

--- c_sources/c_test/uart.c
+++ c_sources/c_test/uart.c
@@ -3,21 +3,32 @@
 
 #define GET_32(x) (*((volatile uint32_t*)(x)))
 
+struct uart_control_t {
+    volatile uint32_t clkdiv;
+    uint32_t padding[3];
+    volatile uint32_t frame;
+    uint32_t padding2[3];
+    volatile uint32_t tx;
+    uint32_t padding3[3];
+    volatile uint32_t rx;
+    uint32_t padding4[3];
+} __attribute__((aligned(16)));
+struct uart_control_t * const uart_ptr = (struct uart_control_t*) UART_BASE;
+
 void uart_setup(void){
-    GET_32(UART_BASE+UART_CLKDIV) = 1;
-    GET_32(UART_BASE+UART_FRAME) = 7;
+    uart_ptr->clkdiv = 1;
+    uart_ptr->frame = 7;
 }
 
 void uart_putc(char c){
-    while((GET_32(UART_BASE + UART_TX) & 0x2) == 0){}
+    while((uart_ptr->tx & 0x2) == 0){}
     
-    GET_32(UART_BASE + UART_TX) = c;
-
+    uart_ptr->tx = c;
 }
 
 void uart_flush(void){
     // wait until the uart queue empties
-    while((GET_32(UART_BASE + UART_TX) & 0x1) == 0){}
+    while((uart_ptr->tx & 0x1) == 0){}
     // wait some extra cycles so it shows up on the console
     for(int i=0; i<200; i++){
 	__asm__ __volatile__("nop");
@@ -32,10 +43,10 @@ void uart_puts(const char *str){
 }
 
 char uart_getc(void){
-    while((GET_32(UART_BASE + UART_TX) & 0x4) == 0){}
+    while((uart_ptr->tx & 0x4) == 0){}
     char c;
     
-    c = GET_32(UART_BASE + UART_RX) & 0xff;
+    c = uart_ptr->rx & 0xff;
 
     return c;
 }
