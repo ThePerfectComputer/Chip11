8382a592853a3f8dc63c1eaf00307562f71ad5bb
Michael Nolan <mtnolan2640@gmail.com>
2021-06-14 11:26:42 -0400

    Begin having the thing read from the memory

--- src/main/scala/soc/devices/memory_adaptor/LineAXIAdaptor.scala
+++ src/main/scala/soc/devices/memory_adaptor/LineAXIAdaptor.scala
@@ -8,7 +8,11 @@ import spinal.lib.bus.amba4.axi.{
   Axi4CrossbarFactory
 }
 // import cpu.debug.debug_memory_adaptor
-import cpu.shared.memory_state.{TransactionStatus, TransactionType, TransactionSize}
+import cpu.shared.memory_state.{
+  TransactionStatus,
+  TransactionType,
+  TransactionSize
+}
 
 import spinal.core._
 import spinal.lib._
@@ -18,7 +22,7 @@ class LineAXIAdaptor(id: Int)(implicit axiConfig: Axi4Config)
     extends Component {
 
   val io = new Bundle {
-    val request  = slave(new LineRequest)
+    val request = slave(new LineRequest)
     val response = master(new LineResponse)
 
     val axi = master(Axi4Shared(axiConfig))
@@ -51,6 +55,37 @@ class LineAXIAdaptor(id: Int)(implicit axiConfig: Axi4Config)
   io.response.data := 0
   io.response.status := TransactionStatus.IDLE
 
+  val req_start_byte = requestReg.byte_address(3 downto 0)
+  val req_size = requestReg.size.as(UInt)
+  // Whether the transaction will have to use two bus accesses
+  val bus_aligned = Bool
+  bus_aligned := False
+  // Whether the transaction address is aligned to the size
+  val byte_aligned = Bool
+  byte_aligned := False
+
+  for (permutation <- LineRequestTruthTable.TableEntries) {
+    when(
+      (req_start_byte === permutation.start_byte) & (req_size === permutation.request_size)
+    ) {
+      bus_aligned := Bool(permutation.bytes_in_transaction2 == 0)
+      byte_aligned := Bool(permutation.byte_addr_aligned)
+    }
+  }
+
+  // The size field presented to the axi bus
+  val axi_size = UInt(3 bits)
+  axi_size := 4
+  when(byte_aligned) {
+    switch(requestReg.size) {
+      is(TransactionSize.BYTE) { axi_size := 0 }
+      is(TransactionSize.HALFWORD) { axi_size := 1 }
+      is(TransactionSize.WORD) { axi_size := 2 }
+      is(TransactionSize.DOUBLEWORD) { axi_size := 3 }
+      is(TransactionSize.QUADWORD) { axi_size := 4 }
+    }
+  }
+
   val fsm = new StateMachine {
     val address1 = new State with EntryPoint
     val address2 = new State
@@ -62,10 +97,34 @@ class LineAXIAdaptor(id: Int)(implicit axiConfig: Axi4Config)
     // Handles the awr channel
     address1.whenIsActive {
       // When valid
-      when(io.request.ldst_req =/= TransactionType.NONE){
+      when(io.request.ldst_req =/= TransactionType.NONE) {
         requestReg := io.request
         io.request.ack := True
         goto(address2)
+      }
+    }
+    address2.whenIsActive {
+      io.axi.arw.addr := requestReg.byte_address
+      io.axi.arw.id := id
+      io.axi.arw.write := requestReg.ldst_req === TransactionType.STORE
+      io.axi.arw.size := axi_size
+      io.axi.arw.burst := 1 // INCR
+      io.axi.arw.len := U(~bus_aligned).resized
+      io.axi.arw.valid := True
+      when(io.axi.arw.ready){
+        when(requestReg.ldst_req === TransactionType.LOAD){
+          when(bus_aligned){
+            goto(read2)
+          }.otherwise {
+            goto(read1)
+          }
+        }.otherwise {
+          when(bus_aligned){
+            goto(write2)
+          }.otherwise {
+            goto(write1)
+          }
+        }
       }
     }
 
--- src/test/scala/soc/LineAXIAdaptorTest.scala
+++ src/test/scala/soc/LineAXIAdaptorTest.scala
@@ -26,6 +26,7 @@ import spinal.lib.bus.amba4.axi._
 import org.scalatest._
 import flatspec._
 import matchers._
+import scala.collection.mutable.ListBuffer
 
 class LineAXIDUT(implicit config: Axi4Config) extends Component {
   val io = new Bundle {
@@ -39,6 +40,14 @@ class LineAXIDUT(implicit config: Axi4Config) extends Component {
     byteCount = ramSize,
     idWidth = 5
   )
+
+  var initialData = new ListBuffer[Bits]
+  val item = BigInt("00000003000000020000000100000000", 16)
+  val increment = BigInt("00000004000000040000000400000004", 16)
+  for(i <- 0 until (ramSize/16)){
+    initialData += B(item + increment*i, 128 bits)
+  }
+  ram.ram.init(initialData)
 
   adaptor.io.request <> io.request
   adaptor.io.response <> io.response
@@ -59,6 +68,30 @@ class LineAXIAdaptorTest extends AnyFlatSpec with should.Matchers {
       dut.clockDomain.forkStimulus(period = 10)
       sleep(50)
 
+      // Send a read request
+      dut.io.request.ldst_req #= TransactionType.LOAD
+      dut.io.request.size #= TransactionSize.WORD
+      dut.io.request.data #= 0
+      dut.io.request.byte_address #= 0x100
+      while (!dut.io.request.ack.toBoolean) {
+        dut.clockDomain.waitSampling(1)
+      }
+      dut.clockDomain.waitSampling(5)
+      // dut.io.request.byte_address #= 0x101
+      // while (!dut.io.request.ack.toBoolean) {
+      //   dut.clockDomain.waitSampling(1)
+      // }
+      // dut.clockDomain.waitSampling(5)
+      // dut.io.request.byte_address #= 0x10c
+      // while (!dut.io.request.ack.toBoolean) {
+      //   dut.clockDomain.waitSampling(1)
+      // }
+      // dut.clockDomain.waitSampling(5)
+      // dut.io.request.byte_address #= 0x10d
+      // while (!dut.io.request.ack.toBoolean) {
+      //   dut.clockDomain.waitSampling(1)
+      // }
+      // dut.clockDomain.waitSampling(5)
     }
   }
 }
